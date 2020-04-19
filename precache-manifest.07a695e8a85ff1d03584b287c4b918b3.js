f404284b4ad84d9ca72df72ce4abef851e1bbe93
Sergio Padilla Villaseñor <sergiolpv97@gmail.com>
2020-04-19 00:18:48 -0700

    Updates

---
+++ precache-manifest.07a695e8a85ff1d03584b287c4b918b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d687085022042452838df242033a8be8",
+    "url": "/sergiopadillav/COVID-19-Tracker/index.html"
+  },
+  {
+    "revision": "bc338e4b3fd48f94b7e9",
+    "url": "/sergiopadillav/COVID-19-Tracker/static/css/main.ed822ac9.chunk.css"
+  },
+  {
+    "revision": "ab0fa1a112346346d80b",
+    "url": "/sergiopadillav/COVID-19-Tracker/static/js/2.8c3b9063.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/sergiopadillav/COVID-19-Tracker/static/js/2.8c3b9063.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc338e4b3fd48f94b7e9",
+    "url": "/sergiopadillav/COVID-19-Tracker/static/js/main.b2a37ea8.chunk.js"
+  },
+  {
+    "revision": "1af2aa49197f199eb9d5",
+    "url": "/sergiopadillav/COVID-19-Tracker/static/js/runtime-main.c4cd581a.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/sergiopadillav/COVID-19-Tracker/static/media/image.d7265326.png"
+  }
+]);
